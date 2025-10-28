4510eadc10b83eec43e0ddd198a694b926fa4767
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-28 19:20:14 +0530

    Update Register component layout and form

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,30 +2,34 @@ import React from "react";
 
 const Register = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-        </div>
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <div className="card-body">
-            <fieldset className="fieldset">
-              <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
-              <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
-              <div>
-                <a className="link link-hover">Forgot password?</a>
-              </div>
-              <button className="btn btn-neutral mt-4">Login</button>
-            </fieldset>
-          </div>
-        </div>
+    <div className="card bg-base-100 w-full mx-auto max-w-sm my-10 shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className="lg:text-3xl font-bold">Plese Register</h1>
+        <form>
+          <fieldset className="fieldset">
+            {/* email field */}
+            <label className="label">Email</label>
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="Email"
+            />
+            {/* password field */}
+            <label className="label">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="input"
+              placeholder="Password"
+            />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-4">Register</button>
+          </fieldset>
+          <p>Already have an account ?</p>
+        </form>
       </div>
     </div>
   );
