def34b67a6242e0d331b7d18a49a6cfb6d01b2ac
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-28 18:59:46 +0530

    Add Register component

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// DANGER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAlfYuNL5pZ9NTJZQzJB5HHC8P7muqn-U",
+  authDomain: "react-firebase-auth-adbaa.firebaseapp.com",
+  projectId: "react-firebase-auth-adbaa",
+  storageBucket: "react-firebase-auth-adbaa.firebasestorage.app",
+  messagingSenderId: "193043656978",
+  appId: "1:193043656978:web:d48fefc8f3d10899ee03cb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
