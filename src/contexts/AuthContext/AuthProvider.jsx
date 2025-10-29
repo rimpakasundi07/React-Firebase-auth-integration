4c81fb0c04c3d303dd8b6b03335e05d198ff0716
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-29 19:10:19 +0530

    Add AuthProvider with Firebase user creation

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import { AuthContext } from "./AuthContext";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/firebase.init";
 
 const AuthProvider = ({ children }) => {
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
   const authInfo = {
-    user: "motu@patlu.com",
+    // createUser: createUser,
+    createUser,
   };
   return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
 
 export default AuthProvider;
+
+/**
+ *
+ *  1. create a context and export
+ *  2. create a provider -- so that you can resuse the context in different places
+ *  3. ensure you use the children prop
+ *  4. make sure use the auth provider in the router
+ *  5. make the create user with email and password shared via provider
+ *  6. set createUser in the authInfo object to share via context
+ */
