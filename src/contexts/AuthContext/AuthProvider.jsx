aaf6354087c81f0a79b8af181a2ce72d1fa35e4f
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-29 21:23:37 +0530

    Implement Login component with signInUser functionality

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { AuthContext } from "./AuthContext";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../../firebase/firebase.init";
 
 const AuthProvider = ({ children }) => {
@@ -8,9 +11,14 @@ const AuthProvider = ({ children }) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
   const authInfo = {
     // createUser: createUser,
     createUser,
+    signInUser,
   };
   return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
